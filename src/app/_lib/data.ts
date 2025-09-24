297852fb34a0f8bcbb4601a20831e0eae8fc28a8
Artem Levchenko <levchencode@gmail.com>
2025-09-24 22:20:26 +0300

    test data

---
+++ src/app/_lib/data.ts
@@ -0,0 +1,32 @@
+import { Level } from "@/types";
+
+export const levels: Level[] = [
+  {
+    id: 1,
+    title: "Weather",
+    link: "/weather",
+    difficulty: "easy",
+    words: ["sun", "rain", "cloud"],
+  },
+  {
+    id: 2,
+    title: "Animals",
+    link: "/animals",
+    difficulty: "medium",
+    words: ["cat", "dog", "bird", "fish", "snake"],
+  },
+  {
+    id: 3,
+    title: "Weather",
+    link: "/weather",
+    difficulty: "easy",
+    words: ["sun", "rain", "cloud"],
+  },
+  {
+    id: 4,
+    title: "Animals",
+    link: "/animals",
+    difficulty: "medium",
+    words: ["cat", "dog", "bird", "fish", "snake"],
+  },
+];
