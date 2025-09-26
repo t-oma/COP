02810bdd00c87c07e3efb07a6000e81fa36c3ab4
Artem Levchenko <levchencode@gmail.com>
2025-09-26 18:01:03 +0300

    stats link

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -2,12 +2,19 @@ import Link from "next/link";
 
 export default function AppHeader() {
   return (
-    <header className="flex flex-col items-center justify-between border-b border-zinc-300 bg-zinc-100 p-4">
+    <header className="flex items-center justify-between border-b border-zinc-300 bg-zinc-100 p-4 px-10">
       <Link
         href="/"
         className="text-xl"
       >
         Word Search
+      </Link>
+
+      <Link
+        href="#/stats"
+        className="text-xl"
+      >
+        Stats
       </Link>
     </header>
   );
