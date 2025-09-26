f9702d7acc2fe48b102ebd4a099cc2bc3af7b80c
Artem Levchenko <levchencode@gmail.com>
2025-09-26 15:21:02 +0300

    sizes

--- src/app/_lib/data.ts
+++ src/app/_lib/data.ts
@@ -1,4 +1,27 @@
-import { Game } from "@/types";
+import {
+  DifficultyNamedSizes as DifficultyNS,
+  Game,
+  NumberNamedSizes as NumberNS,
+  Size,
+} from "@/types";
+
+export const Sizes: Size[] = [
+  { width: 5, height: 5 },
+  { width: 7, height: 7 },
+  { width: 9, height: 9 },
+];
+
+export const NumberNamedSizes: NumberNS = {
+  "5x5": Sizes[0],
+  "7x7": Sizes[1],
+  "9x9": Sizes[2],
+};
+
+export const DifficultyNamedSizes: DifficultyNS = {
+  easy: /*  */ Sizes[0],
+  medium: /**/ Sizes[1],
+  hard: /*  */ Sizes[2],
+};
 
 export const games: Game[] = [
   {
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,22 @@
 export type Difficulty = "easy" | "medium" | "hard";
 
+export type Size = {
+  width: number;
+  height: number;
+};
+
+export type NumberNamedSizes = {
+  "5x5": Size;
+  "7x7": Size;
+  "9x9": Size;
+};
+
+export type DifficultyNamedSizes = {
+  easy: Size;
+  medium: Size;
+  hard: Size;
+};
+
 export type Game = {
   id: number;
   title: string;
