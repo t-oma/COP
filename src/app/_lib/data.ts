24262e2000bb1bfed4b9b88ebdd58a5be99c5d92
Artem Levchenko <levchencode@gmail.com>
2025-09-26 16:10:05 +0300

    remove word from easy

--- src/app/_lib/data.ts
+++ src/app/_lib/data.ts
@@ -29,7 +29,7 @@ export const games: Game[] = [
     title: "Weather",
     link: "/weather",
     difficulty: "easy",
-    words: ["sun", "rain", "snow", "thunder", "cloud"],
+    words: ["sun", "rain", "snow", "cloud"],
   },
   {
     id: 2,
@@ -43,7 +43,7 @@ export const games: Game[] = [
     title: "Weather",
     link: "/weather",
     difficulty: "easy",
-    words: ["sun", "rain", "snow", "thunder", "cloud"],
+    words: ["sun", "rain", "snow", "cloud"],
   },
   {
     id: 4,
