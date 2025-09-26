10089ad06612f3dc92369cc3a684aead735e0e00
Artem Levchenko <levchencode@gmail.com>
2025-09-26 18:01:08 +0300

    styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     <>
       <AppHeader />
 
-      <main className="flex flex-1 flex-col px-10">
+      <main className="flex flex-1 flex-col px-10 pb-14">
         <div className="flex items-center py-4">
           <h1 className="text-xl font-semibold">Categories</h1>
         </div>
