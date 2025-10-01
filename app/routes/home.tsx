ba79c949af22b4b723a6c95396aaa6d13adc5dee
Artem Levchenko <levchencode@gmail.com>
2025-10-01 10:08:38 +0300

    basic layout

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { AppFooter, AppHeader } from "~/components";
 import type { Route } from "./+types/home";
 
 // eslint-disable-next-line
@@ -9,5 +10,19 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function HomePage() {
-  return <>Hello</>;
+  return (
+    <>
+      <AppHeader />
+
+      <main className="flex flex-1 flex-col px-10 pb-14">
+        <div className="flex items-center py-4">
+          <h1 className="text-xl font-semibold">Categories</h1>
+        </div>
+
+        <div></div>
+      </main>
+
+      <AppFooter />
+    </>
+  );
 }
