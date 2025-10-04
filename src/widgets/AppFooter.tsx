963106ca1e8948afe3416108bdfb601f585b251c
Artem Levchenko <levchencode@gmail.com>
2025-10-04 18:00:39 +0300

    styles

--- src/widgets/AppFooter.tsx
+++ src/widgets/AppFooter.tsx
@@ -1,6 +1,6 @@
 function AppFooter() {
   return (
-    <footer className="flex items-center justify-center border-t border-zinc-300 bg-zinc-100 p-4">
+    <footer className="flex items-center justify-center border-t border-zinc-300 bg-accent-background p-4">
       <p>Levchenko Artem</p>
     </footer>
   );
--- src/widgets/AppHeader.tsx
+++ src/widgets/AppHeader.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 function AppHeader() {
   return (
-    <header className="flex items-center justify-between border-b border-zinc-300 bg-zinc-100 p-4 px-10">
+    <header className="flex items-center justify-between border-b border-zinc-300 bg-accent-background p-4 px-10">
       <Link to="/" className="text-xl">
         Word Search
       </Link>
