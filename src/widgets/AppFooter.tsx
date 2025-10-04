0cbb67f1418b1c6fa59ef1a3e31512b5f72b67a7
Artem Levchenko <levchencode@gmail.com>
2025-10-04 12:58:49 +0300

    renamed to PascalCase

---
+++ src/widgets/AppFooter.tsx
@@ -0,0 +1,9 @@
+function AppFooter() {
+  return (
+    <footer className="flex items-center justify-center border-t border-zinc-300 bg-zinc-100 p-4">
+      <p>Levchenko Artem</p>
+    </footer>
+  );
+}
+
+export { AppFooter };
