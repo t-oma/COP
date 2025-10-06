0e111e64c145253a86f11b4c17082ee411b38c85
Artem Levchenko <levchencode@gmail.com>
2025-10-06 14:45:26 +0300

    stats route

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,4 +3,5 @@ import { index, route, type RouteConfig } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("/games/:id", "./routes/game.tsx"),
+  route("/stats", "./routes/stats.tsx"),
 ] satisfies RouteConfig;
---
+++ src/pages/stats/stats.tsx
@@ -0,0 +1,11 @@
+import { RootLayout } from "~/widgets";
+
+export default function StatsPage() {
+  return (
+    <RootLayout>
+      <main className="flex flex-1">
+        <h1>Stats</h1>
+      </main>
+    </RootLayout>
+  );
+}
