69f40ba1b047c4c1911c8484207fc699c1b3176d
Artem Levchenko <levchencode@gmail.com>
2025-10-07 16:55:39 +0300

    timer component

---
+++ src/widgets/GameTimer.tsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useTimer } from "~/shared/hooks";
+
+interface GameTimerProps {
+  autoStart?: boolean;
+}
+
+function GameTimer({ autoStart = false }: Readonly<GameTimerProps>) {
+  const timer = useTimer();
+
+  useEffect(() => {
+    if (autoStart) {
+      timer.start();
+    }
+  }, [timer, autoStart]);
+
+  return (
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col items-center justify-center gap-1">
+        <span className="font-mono text-2xl font-bold text-zinc-900">
+          {timer.formatTime()}
+        </span>
+        <span className="text-sm text-zinc-500">Time</span>
+      </div>
+    </div>
+  );
+}
+
+export { GameTimer };
