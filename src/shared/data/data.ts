87c5586cc1e409bab21650a0aa4538bf0f26987a
Artem Levchenko <levchencode@gmail.com>
2025-10-15 21:27:16 +0300

    freeze data

--- src/shared/data/data.ts
+++ src/shared/data/data.ts
@@ -4,20 +4,20 @@ import type {
   Size,
 } from "~/shared/types";
 
-export const Sizes: Size[] = [
-  { width: 5, height: 5 },
-  { width: 7, height: 7 },
-  { width: 9, height: 9 },
-] as const;
+export const Sizes: readonly Size[] = Object.freeze([
+  Object.freeze({ width: 5, height: 5 } as const),
+  Object.freeze({ width: 7, height: 7 } as const),
+  Object.freeze({ width: 9, height: 9 } as const),
+] as const);
 
-export const NumberNamedSizes: NumberNS = {
+export const NumberNamedSizes: NumberNS = Object.freeze({
   "5x5": Sizes[0],
   "7x7": Sizes[1],
   "9x9": Sizes[2],
-} as const;
+} as const);
 
-export const DifficultyNamedSizes: DifficultyNS = {
+export const DifficultyNamedSizes: DifficultyNS = Object.freeze({
   easy: /*  */ Sizes[0],
   medium: /**/ Sizes[1],
   hard: /*  */ Sizes[2],
-} as const;
+} as const);
